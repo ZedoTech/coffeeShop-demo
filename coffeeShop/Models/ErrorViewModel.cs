c5e27eb33ab5d14d10fbe2092ad08d980b662072
Nicky Liu <nicky@zedotech.com>
2024-05-26 09:34:48 +0800

    加入專案檔案。

---
+++ coffeeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace coffeeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
